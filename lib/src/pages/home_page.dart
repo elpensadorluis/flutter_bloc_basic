e5883e11f5bf273323242872188f697e0d873910
Luis Guillermo Echenique <elpensadorluis@gmail.com>
2020-04-28 19:27:34 -0400

    Correcciones menores

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -11,7 +11,7 @@ class HomePage extends StatelessWidget {
 
     return Scaffold(
       appBar: AppBar(
-        title: Text('SENTU Studios Settings'),
+        title: Text('SENTU Studio Settings'),
       ),
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.center,
